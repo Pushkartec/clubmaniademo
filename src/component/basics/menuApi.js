4764a9fa1d954b7e656cbd210cad09427eea4c7a
Pushkartec <sareenpushkar46@gmail.com>
2021-10-09 11:33:01 +0200

    my first commit reactwebsite

---
+++ src/component/basics/menuApi.js
@@ -0,0 +1,87 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/klar.jpg",
+      name: "Klar cocktail bar",
+      category: "Bar",
+      price: "12₹",
+      description:
+        "Each day team of bartenders explore available seasonal products to bring the most natural experience of drinking a cocktail or infused vodka. We believe  that limitations coming from working only with natural products brings more creativity than taking the easy way. ",
+    },
+  
+    {
+      id: 2,
+      image: "images/level27.jpg",
+      name: "Level27",
+      category: "Club",
+      price: "20₹",
+      description:
+        "level 27 is a stylish venue located on the 27th floor in the heart of Warsaw, Poland, offering social & cultural events as well as dance and music performances.",
+    },
+    {
+      id: 3,
+      image: "images/skybar.png",
+      name: "Panaroma skybar",
+      category: "Bar",
+      price: "10₹",
+      description:
+        "Panorama Sky Bar is located on the 40th floor of the Marriott hotel, from which our guests can admire the breathtaking views of Warsaw.  ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "Injachi",
+      category: "Restaurant",
+      price: "50₹",
+      description:
+        " Intimate venue serving creative Italian eats such as pasta, mussels & meat dishes, plus wine.",
+    },
+    {
+      id: 5,
+      image: "../images/clubroom.jpg",
+      name: "Club Room 13",
+      category: "Club",
+      price: "80₹",
+      description:
+        "Room 13 to serce klubowego zagłębia przy legendarnej ulicy Mazowieckiej. Doskonała lokalizacja w centrum Warszawy ",
+    },
+    {
+      id: 6,
+      image: "../images/cityfit.jpg",
+      name: "Cityfit",
+      category: "Gym",
+      price: "180₹",
+      description:
+        "We’re happy you’re here. Are you looking for information about us? We’ll tell you all about it in a minute.",
+    },
+    {
+      id: 7,
+      image: "../images/zdrofit.jpg",
+      name: "Zdrofit",
+      category: "Gym",
+      price: "60₹",
+      description:
+        "  Nasz kameralny osiedlowy klub na warszawskim Ursusie wyróżnia przyjazna i swobodna atmosfera",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Restauracja rozna",
+      category: "Restaurant",
+      price: "60₹",
+      description:
+        "Rozna, in ancient Indian Sanskrit language means ‘Knowledge’. The knowledge of life ",
+    },
+    {
+      id: 9,
+      image: "../images/luzztro.jpg",
+      name: "Luzztro",
+      category: "Club",
+      price: "10₹",
+      description:
+        "club luzztro is the best in warsaw check out our page  ",
+    },
+  ];
+  
+  export default Menu;
+  
